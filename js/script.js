b2daea67038e1ac65f0ff3518d031595ccc646f7
Josie <satr4755@bht-berlin.de>
2025-09-24 03:11:55 +0200

    ist nicht responsive!!!!!

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    navLinks.forEach(l => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const header = document.querySelector('header');
+
+    const headerOffset = header.offsetHeight; // Höhe des Headers
+    const extraPadding = 20; // zusätzliches Padding nach Header in px
+
+    const elementPosition = target.offsetTop;
+    const offsetPosition = elementPosition - headerOffset - extraPadding;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
